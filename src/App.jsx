7ebadf0586d21f4dc3deb3aedfd683e0217b5ed2
Amy Gori <amydgori@gmail.com>
2023-12-14 11:44:43 -0500

    Add react router and initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { useEffect } from 'react'
 import Login from './components/Login'
 import useLocalStorageState from 'use-local-storage-state'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   const [questions, setQuestions] = useState([])
@@ -24,17 +25,62 @@ function App() {
   return (
     <>
       <h1>Questions</h1>
-      {token ? (
-        <ul>
-          {questions.map((question) => {
-            return <li key={question.id}>{question.title}</li>
-          })}
-        </ul>
-      ) : (
-        <Login setAuth={setAuth} />
-      )}
+      <Routes>
+        <Route path="/" element={<h2>Homepage</h2>} />
+        <Route
+          path="/questions"
+          element={<QuestionList questions={questions} />}
+        />
+        {/* <Route path="questions/:id" element={}> */}
+        <Route
+          path="questions/mine"
+          element={token ? <QuestionUserList token={token} username={username} /> : <Login />}
+        />
+        <Route path="/login" element={<Login />}
+        <Route path="*" element={<h2>404</h2>} />
+      </Routes>
     </>
   )
 }
 
+const QuestionUserList = ({ username, token }) => {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://qb.fly.dev/questions/me', {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      })
+      .then((res) => {
+        setQuestions(res.data)
+      })
+  }, [token])
+
+  return (
+    <>
+      <h2> Questions by {username} </h2>
+      <ul>
+        {questions.map((question) => {
+          return <li key={question.id}>{question.title}</li>
+        })}
+      </ul>
+    </>
+  )
+}
+
+const QuestionList = ({ questions }) => {
+  return (
+    <ul>
+      {questions.map((question) => {
+        return <li key={question.id}>{question.title}</li>
+      })}
+    </ul>
+  )
+}
+
+const QuestionDetail = () => {
+  return <h1> Question Detail page</h1>
+}
 export default App
